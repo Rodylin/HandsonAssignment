877d8d123d273258086655404e5f1cdbfe89aee1
BædBlood⊹ <rodylinbernales339@gmail.com>
2024-06-26 13:14:58 +0800

    first commit

--- resources/views/templates/base.blade.php
+++ resources/views/templates/base.blade.php
@@ -14,7 +14,7 @@
         <!-- Sidebar -->
         <aside class="w-64 h-screen bg-gradient-to-r from-gray-600 to-blue-900 text-white shadow-lg">
             <div id="brand" class="text-2xl font-bold p-4">
-                HTMX
+                June21
             </div>
             <nav id="main-nav" class="flex flex-col space-y-4 p-4">
                 <a href="/home" class="px-3 py-2 rounded hover:bg-indigo-500 transition duration-300">Home</a>
@@ -27,13 +27,13 @@
         <!-- Main content -->
         <div class="flex-1 p-6">
             <section class="bg-white p-6 rounded shadow-lg">
-                <h1 class="text-3xl font-bold mb-4">HANDS-ON </h1>
-                <p class="text-gray-700">JAMES MANTE</p>
+                <h1 class="text-3xl font-bold mb-4">HANDS-ON</h1>
+                <p class="text-gray-700">RodylinBernales</p>
             </section>
 
             <div class="container mx-auto py-4">
                 @yield('content')
-            </div>
+            </div>  
         </div>
     </div>
 </body>
